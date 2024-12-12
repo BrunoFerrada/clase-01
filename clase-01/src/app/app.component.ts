b6c303603d086af35ee65fbff7d6f3c8311c55d0
BrunoFerrada <brunoferrada1212@gmail.com>
2024-12-11 23:26:42 -0300

    primeros modulos (clase-2)

--- clase-01/src/app/app.component.ts
+++ clase-01/src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'clase-01';
+  title = 'Hola mundo!';
+  userName = 'Bruno';
+  userAge = '23';
 }
